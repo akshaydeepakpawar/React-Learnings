26fbaf9fb7a150c9ac0156e06bfa2b7c201d992f
akshay pawar <ap3690700@gmail.com>
2025-12-11 02:36:20 +0530

    React completed and did 1 small project todo app

---
+++ demo-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoApp from './components/TodoApp'
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-100 pt-10">
+      <TodoApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ demo-project/src/components/TodoApp.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const TodoApp = () => {
+
+    const [todos,setTodo]=useState([]);
+    const [input,setInput]=useState("");
+
+    const submitHandler =()=>{
+        setTodo((todos)=>{
+            return todos.concat({
+                text:input,
+                id: Math.floor(Math.random() * 1000)
+            })
+        })
+        setInput("")
+    }
+    const removeTodo=(id)=>{
+        setTodo(todos=>todos.filter(t=>t.id!=id))
+    }
+
+
+  return (
+    <>
+    <div className='border mt-5 mx-auto p-4 w-[50%] rounded-md flex justify-center items-center gap-5 '>
+        <input type="text" className='border px-5 py-2 rounded-xl'placeholder='Enter your TODO' value={input} 
+        onChange={(e)=>setInput(e.target.value)}/>
+        <button className='border p-1 rounded-md cursor-pointer' onClick={submitHandler}>Add todo</button>
+    </div>
+    <ul className="mt-5 w-[50%] mx-auto space-y-3">
+        {todos.map(({text,id})=><li className="border px-4 py-2 rounded-lg shadow-sm bg-white flex justify-between items-center" key={id}>
+            <span>
+             {text}
+            </span>
+            <button className='border rounded-4xl p-2 cursor-pointer bg-red-500 text-white' onClick={()=>removeTodo(id)}>X</button>
+            </li>)}
+    </ul>
+    </>
+  )
+}
+
+export default TodoApp
